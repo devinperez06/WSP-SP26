81af2c7d4ff1c8ed7ff8f6f58716a47a03a6f56b
Devin Perez <dp4110895@gmail.com>
2026-03-11 13:43:34 -0400

    More formatting

--- client/src/views/ProductList.vue
+++ client/src/views/ProductList.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-import { userProductStore } from '../stores/product';
-const products = userProductStore();
+import { userProductStore } from '../stores/product'
+const products = userProductStore()
 </script>
 
 <template>
   <h1 class="title is-1">Product List</h1>
   <div class="grid is-min-col-10">
-      <div v-for="product in products.products" :key="product.id" class="box">
-          <img :src="product.thumbnail" alt="Product Image" class="image is-4by3">
-          <button class="button is-primary is-small add-button">Add to Cart</button>
-          <b>{{ product.title}}</b>
-          {{ product.description }}
-          <div class="price">${{ product.price }}</div>
-      </div>
+    <div v-for="product in products.products" :key="product.id" class="box">
+      <img :src="product.thumbnail" alt="Product Image" class="image is-4by3" />
+      <button class="button is-primary is-small add-button">Add to Cart</button>
+      <b>{{ product.title }}</b>
+      {{ product.description }}
+      <div class="price">${{ product.price }}</div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-  .add-button{
-    float: right;
-    margin-top: .5em;
-  }
+.add-button {
+  float: right;
+  margin-top: 0.5em;
+}
 </style>
